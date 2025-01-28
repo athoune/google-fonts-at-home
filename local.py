dd78f0258e8bad9a02c55c01de579c6a14e9d5f6
Mathieu Lecarme <mathieu@garambrogne.net>
2025-01-28 19:45:04 +0100

    Write the new CSS file.

--- local.py
+++ local.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import io
+from pathlib import Path
 import re
 import subprocess
 
@@ -13,24 +14,56 @@ from tinycss2.ast import (
     FunctionBlock,
     IdentToken,
     NumberToken,
+    URLToken,
+    StringToken,
 )
 
 BAD = re.compile(r"[. ]+")
 
 
-def slurp(url: str):
-    resp = requests.get(url)
+def slurp(google_css_url: str, my_url_prefix: str = ""):
+    my_url_prefix = my_url_prefix.rstrip("/")
+    resp = requests.get(google_css_url)
     assert resp.status_code == 200
     rules = tinycss2.parse_stylesheet(
         resp.content.decode("utf-8"), skip_comments=True, skip_whitespace=True
     )
+    sources = []
+    names = []
     for rule in rules:
         block = dict(line for line in handle_rule(rule))
         name = file_name(block)
-        url = block["src"][0].value
         print(name)
-        download(url, name)
-        subprocess.run(["woff2_compress", name], check=True)
+        names.append(name)
+        sources.append(block["src"][0])
+        if not Path(name).exists():
+            google_css_url = block["src"][0].value
+            download(google_css_url, name)
+        if not Path(name[:-3] + "woff2").exists():
+            subprocess.run(["woff2_compress", name], check=True, capture_output=True)
+
+    names.reverse()
+    for rule in rules:
+        patched = []
+        for c in rule.content:
+            if isinstance(c, URLToken):
+                patched.append(WhitespaceToken(0, 0, "\n    "))
+                name = names.pop()
+                value = "%s/%s" % (my_url_prefix, name[:-3] + "woff2")
+                patched.append(URLToken(0, 0, value, "url(%s)" % value))
+                patched.append(WhitespaceToken(0, 0, " "))
+                patched.append(
+                    FunctionBlock(0, 0, "format", [StringToken(0, 0, "woff2", "woff2")])
+                )
+                patched.append(LiteralToken(0, 0, ","))
+                patched.append(WhitespaceToken(0, 0, "\n    "))
+                c.value = "%s/%s" % (my_url_prefix, name)
+                c.representation = "url(%s)" % c.value
+            patched.append(c)
+        rule.content = patched
+
+    with open("index.css", "w") as output:
+        output.write(tinycss2.serialize(rules))
 
 
 def file_name(block: dict):
@@ -81,5 +114,6 @@ def cut(content):
 
 if __name__ == "__main__":
     slurp(
-        "https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+        "https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap",
+        "https://font.example.com",
     )
